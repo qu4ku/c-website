0d04be11dcc28c2c95a26e3efc8fd7ec9b20d8f5
qu4ku <qu4ku@hotmail.com>
2018-03-15 04:28:12 +0100

    crypt

--- pages/views.py
+++ pages/views.py
@@ -21,7 +21,7 @@ def home_view(request):
 			Q(description__icontains=query)
 		).distinct()
 
-	paginator = Paginator(post_list, 20)  # Show 25 contacts per page
+	paginator = Paginator(post_list, 18)
 	page = request.GET.get('page')
 	posts = paginator.get_page(page)
 
@@ -35,6 +35,10 @@ def home_view(request):
 
 def about_view(request):
 	return render(request, 'about.html')
+
+
+def jobs_view(request):
+	return render(request, 'jobs.html')
 
 
 def post_detail_view(request, slug):
@@ -91,6 +95,7 @@ def post_delete_view(request, slug=None):
 	messages.success(request, 'Deleted')
 	return redirect('home')
 
+
 def category_view(request, slug):
 	category = get_object_or_404(Category, slug=slug)
 	post_list = Post.objects.published().filter(categories=category).order_by('-publish')
@@ -106,17 +111,42 @@ def category_view(request, slug):
 
 	return render(request, template, context)
 
+
 def level_view(request, slug):
 	level = get_object_or_404(DifficultyLevel, difficulty_level=slug.title())
 	post_list = Post.objects.published().filter(difficulty_level=level).order_by('-publish')
 
-	paginator = Paginator(post_list, 20)  # Show 25 contacts per page
+	paginator = Paginator(post_list, 18)
 	page = request.GET.get('page')
 	posts = paginator.get_page(page)
 
 	template = 'home.html'
 	context = {
 		'posts': posts,
+	}
+
+	return render(request, template, context)
+
+
+def search_view(request):
+	post_list = Post.objects.published().order_by('-publish')
+	query = request.GET.get('q')  # Search
+
+	if query:
+		# Check both title and description for the query word
+		post_list = Post.objects.published().filter(
+			Q(title__icontains=query) | 
+			Q(description__icontains=query)
+		).distinct()
+
+	paginator = Paginator(post_list, 18)
+	page = request.GET.get('page')
+	posts = paginator.get_page(page)
+
+	template = 'search_results.html'
+	context = {
+		'posts': posts,
+		'query': query,
 	}
 
 	return render(request, template, context)
