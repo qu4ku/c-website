07b8de5ee7ba0b7755c91a6196dfc690d6f36d2d
qu4ku <qu4ku@hotmail.com>
2018-03-18 03:30:48 +0100

    password reset

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import (
+	login, logout, password_reset, password_reset_done, password_reset_confirm, 
+	password_reset_complete
+)
 
 from . import views
 
@@ -10,5 +13,10 @@ urlpatterns = [
 	path('register/', views.register_view, name='register'),
 	path('profile/', views.profile_view, name='profile'),
 	path('profile/edit/', views.profile_edit_view, name='profile_edit'),
-	path('profile/change-password/', views.change_password_view, name='change_password'),
+	path('change-password/', views.change_password_view, name='password_change'),
+	path('reset-password/', password_reset, name='password_reset'),
+	# Need to use standard django name here
+	path('reset-password/done/', password_reset_done, name='password_reset_done'),
+	path('reset-password/confirm/', password_reset_confirm, name='password_reset_confirm'),
+	path('reset-password/complete/', password_reset_complete, name='password_reset_complete'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
 
 from .forms import RegistrationForm, EditProfileForm
 
@@ -30,14 +31,35 @@ def profile_view(request):
 def profile_edit_view(request):
 	if request.method == 'POST':
 		form = EditProfileForm(request.POST, instance=request.user)
-		
+
 		if form.is_valid():
 			form.save()
 
 			return redirect('profile')
 	else:
 		form = EditProfileForm(instance=request.user)
+		print(form)
 		context = {'form': form}
 		template = 'profile_edit.html'
 
 		return render(request, template, context)
+
+
+def change_password_view(request):
+	if request.method == 'POST':
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+		print(form)
+		if form.is_valid():
+			print('valid')
+			form.save()
+			update_session_auth_hash(request, form.user)
+			return redirect('profile')
+		else:
+			print('invalid')
+			return redirect('password_change')
+
+	form = PasswordChangeForm(user=request.user)
+	context = {'form': form}
+	template = 'change_password.html'
+
+	return render(request, template, context)
