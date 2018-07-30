a43f29de57619dcf820675b0a843db66929a96e8
qu4ku <qu4ku@hotmail.com>
2018-07-30 12:34:52 +0200

    categories box size in admin

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Post, DifficultyLevel, PostType, Category, Link, Feedback, ReviewedLink
 
+
+class PostAdminForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = '__all__'
+		widgets = {
+			'categories': forms.SelectMultiple(attrs={'size': 20}),
+		}
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'set_number', 'publish', 'status', 'post_type']
 	list_editable = ['status','set_number', 'publish']
 	list_filter = ['status', 'publish', 'post_type', 'difficulty_level', 'categories']
 	search_fields = ['title', 'description']
-	list_per_page = 30
+	list_per_page = 28
+
+	form = PostAdminForm
 
 	class Meta:
 		model = Post
