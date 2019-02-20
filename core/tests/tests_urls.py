11624905a6051e017eda7e07ca5e2bbb44b9c9d8
qu4ku <qu4ku@hotmail.com>
2019-02-20 05:13:42 +0100

    tests init

--- core/tests/tests_urls.py
+++ core/tests/tests_urls.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from django.test import SimpleTestCase
 
 
-class TestConnect(TestCase):
+class TestConnections(TestCase):
+
 	def test_home_page_status_code(self):
 		response = self.client.get('/')
 		self.assertEquals(response.status_code, 200)
@@ -10,3 +11,7 @@ class TestConnect(TestCase):
 	def test_about_page_status_code(self):
 		response = self.client.get('/about/')
 		self.assertEquals(response.status_code, 200)
+
+
+class TestUrls(SimpleTestCase):
+	assert 1 == 2
--- core/views.py
+++ core/views.py
@@ -41,18 +41,18 @@ def about_view(request):
 def post_detail_view(request, slug):
 	template = 'post_detail.html'
 	post = get_object_or_404(Post, slug=slug, status='public')
-	categories = post.categories.all() # Get categories objects
+	tags = post.tags.all() # Get categories objects
 	
-	categories_dict = {}
+	tags_dict = {}
 	post_cap = 3
-	for category in categories:
+	for tag in tags:
 
-		top_posts =  Post.objects.all().filter(categories=category)[:post_cap]
-		categories_dict[category] = top_posts
+		top_posts =  Post.objects.all().filter(tags=tag)[:post_cap]
+		tags_dict[tag] = top_posts
 
 	context = {
 		'post': post,
-		'categories': categories_dict,
+		'categories': tags_dict,
 		}
 
 	return render(request, template, context)
